59c130a15bf3b7430bd25fbda08ca1d941bbc98c
polinasateesh-vox <sateesh@voxglobaltech.com>
2024-07-05 12:48:09 +0530

    added update and delete User

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -2,14 +2,14 @@ const User = require("../model/userModal");
 
 // Create a new user
 const createUser = async (req, res) => {
-  console.log('req',req.body)
+  console.log("req", req.body);
   try {
     const user = new User(req.body);
     await user.save();
-    res.status(201).json({ message: 'User created successfully', data: user });
+    res.status(201).json({ message: "User created successfully", data: user });
   } catch (error) {
-    console.log('error:',error)
-    res.status(500).json({ message: 'Internal server error' });
+    console.log("error:", error);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
@@ -18,52 +18,92 @@ const getUsers = async (req, res) => {
   try {
     const users = await User.find();
     if (users.length === 0) {
-      res.status(404).json({ message: 'No users found' });
+      res.status(404).json({ message: "No users found" });
     } else {
-      res.status(200).json({ message: 'Users retrieved successfully', data: users });
+      res
+        .status(200)
+        .json({ message: "Users retrieved successfully", data: users });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
-
 
 // Get user by Id
 const getUserById = async (req, res) => {
-
   try {
     const user = await User.findById(req.params.id);
-    console.log('user',user)
+    console.log("user", user);
     if (user.length === 0) {
-      res.status(404).json({ message: 'No user found' });
+      res.status(404).json({ message: "No user found" });
     } else {
-      res.status(200).json({ message: 'User retrieved successfully', data: user });
+      res
+        .status(200)
+        .json({ message: "User retrieved successfully", data: user });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 };
 
-
-const updateUser=async(req,res)=>{
-  console.log('req.params.id',req.params.id)
-  console.log('req.body',req.body)
-  try{
-    const updatedUser=User.findByIdAndUpdate(req.params.id,req.body,{new:true,runValidators: true})
-    console.log("updatedUser",updatedUser)
+const updateUser = async (req, res) => {
+  try {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: false,
+    });
+    console.log("updatedUser", updatedUser);
     if (!updatedUser) {
-      return res.status(404).json({ message: 'No user found' });
+      res.status(404).json({ message: "No user found" });
     }
-    res.status(200).json({message:"User Updated successful",data:updatedUser})
-  }catch(error){
-    console.log('error',error)
+    res
+      .status(200)
+      .json({ message: "User updated successfully", data: updatedUser });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const updateUserPatch = async (req, res) => {
+  console.log("req.params.id", req.params.id);
+  console.log("req.body", req.body);
+  try {
+    const updatedUser =  await User.findByIdAndUpdate(req.params.id, {
+     firstName: req.body.firstName, 
+    },{new:true});
+
+    console.log('updatedUser',updatedUser)
+    if (!updatedUser) {
+      res.status(404).json({ message: "user Not Found" });
+    }
+
+    res
+      .status(200)
+      .json({ message: "User updated successfully", data: updatedUser });
+  } catch (error) {
+    console.log("Error:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+const deleteUser = async (req, res) => {  
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      res.status(404).json({ message: "No user found" });
+    }
+    res
+     .status(200)
+     .json({ message: "User deleted successfully"});
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error" });
   }
 }
-module.exports = {
-    createUser,
-    getUsers,
-    getUserById,
-    updateUser
-  };
 
-  
+module.exports = {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUser,
+  updateUserPatch,
+  deleteUser
+};
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,7 +7,7 @@ const userController=require('../controller/user.controller')
 router.post('/createUser', userController.createUser)
 router.get('/getUser', userController.getUsers)
 router.get('/getUser/:id', userController.getUserById)
-router.put('/updateUser/:id',userController.updateUser)
-
-
+router.put('/updateUser/:id', userController.updateUser)
+router.delete('/deleteUser/:id', userController.deleteUser)
+router.patch('/updateUserPatch/:id', userController.updateUserPatch)
 module.exports=router
